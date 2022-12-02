ded67fcce8fb4be0a0b09c224f89df58bc7f3726
10ge6 <dsf@jugemu.systems>
2022-12-02 15:00:57 -0300

    4~5 standalone

--- src/standalone/proj.s
+++ src/standalone/proj.s
@@ -5,6 +5,9 @@ frase: db "Qual e a importancia da escola na democratizacao da sociedade", 0h
 
 section .bss
 msg: resb 41                    ; reservar area na memoria com comprimento da string desejada ("a importancia...democratizacao")
+inv: resb 41
+concat: resb 41
+caps: resb 41
 
 section .text
 global _start
@@ -14,6 +17,8 @@ _start:
     call Q1
     call Q2
     call Q3
+    call Q4
+    call Q5
     
     call sysquit
 
@@ -68,12 +73,87 @@ Q3INVRT:
     cmp     ecx,    6           ; checar se estamos no comeco da string - 1 (o espaco antes do primeiro 'a')
     je      Q3FIM               ; caso sim, terminar
     mov     al,    [frase+ecx]  ; mov [Q3MSG+ebx], [frase+ecx] nao pode ser feito diretamente, portanto
-    mov     [msg+ebx], al       ; utilizamos al como buffer para realizar essa troca, um byte por vez
+    mov     [inv+ebx], al       ; utilizamos al como buffer para realizar essa troca, um byte por vez
     inc ebx                     ; proximo caractere a ser colocado na string a imprimir
     dec ecx                     ; caractere anterior a ser lido da string normal
     jmp Q3INVRT
 
 Q3FIM:
-    mov     eax,    msg
+    mov     eax,    inv
+    call    strecho
+    ret
+
+
+Q4:
+    xor     ebx,    ebx         ; zerar para utilizar como counter de indice
+    xor     ecx,    ecx         ; das strings origem e concatenada
+    
+Q4INICIO:
+    cmp     ecx,    41          ; checar se no fim da string origem
+    je      Q4FIM
+    cmp     [msg+ecx], byte 20h ; char ' '?
+    je      Q4IGUAL
+    mov     al,    [msg+ecx]    ; mesma ideia, utilizar
+    mov     [concat+ebx], al    ; registrador como buffer
+    inc     ebx                 ; incrementar
+    inc     ecx                 ; indices
+    jmp     Q4INICIO
+    
+Q4IGUAL:
+    inc     ecx                 ; pular whitespace
+    jmp     Q4INICIO
+    
+Q4FIM:
+    mov     eax,    concat
+    call strecho
+    ret
+
+
+Q5:
+    xor     ecx,    ecx
+    xor     ebx,    ebx
+    
+Q5UPPER:
+    cmp     ecx,    41
+    je      Q5FIM
+    cmp     [msg+ecx], byte 20h ; char ' '?
+    je      Q5PULARESPACOU
+    mov     al,     [msg+ecx]
+    sub     al,     32
+    mov     [caps+ecx],   al
+    inc     ecx
+    inc     ebx
+    cmp     ebx,    2
+    jne     Q5UPPER
+    xor     ebx,    ebx
+    
+Q5LOWER:
+    cmp     ecx,    41
+    je      Q5FIM
+    cmp     [msg+ecx], byte 20h
+    je      Q5PULARESPACOL
+    mov     al,     [msg+ecx]
+    mov     [caps+ecx],   al
+    inc     ecx
+    inc     ebx
+    cmp     ebx,    3
+    jne     Q5LOWER
+    xor     ebx,    ebx
+    jmp     Q5UPPER
+    
+Q5PULARESPACOU:
+    mov     al,     [msg+ecx]
+    mov     [caps+ecx],   al
+    inc     ecx
+    jmp     Q5UPPER
+    
+Q5PULARESPACOL:
+    mov     al,     [msg+ecx]
+    mov     [caps+ecx],   al
+    inc     ecx
+    jmp     Q5LOWER
+    
+Q5FIM:
+    mov     eax,    caps
     call    strecho
     ret
