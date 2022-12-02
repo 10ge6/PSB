5547edff48e3cacbd7226377a8331bfa8f6ee9aa
10ge6 <dsf@jugemu.systems>
2022-12-02 04:48:34 -0300

    doc, comm refactor

--- src/standalone/proj.s
+++ src/standalone/proj.s
@@ -4,7 +4,7 @@ section .data
 frase: db "Qual e a importancia da escola na democratizacao da sociedade", 0h
 
 section .bss
-msg: resb 41    ; reservar area na memoria com comprimento da string desejada ("a importancia...democratizacao")
+msg: resb 41                    ; reservar area na memoria com comprimento da string desejada ("a importancia...democratizacao")
 
 section .text
 global _start
@@ -37,7 +37,7 @@ Q2:
 DENOVOA:
     repne   scasb               ; percorre a string (repne termina quando ECX = 0; scasb le de EDI e compara contra EAX, muda EDI considerando DF. ZF = 0 quando a comparacao e diferente! por
                                 ; isso que caso exista a possibilidade da string terminar com o caractere sendo procurado, deve haver um jcxz antes do ultimo jmp; evitar um loop infinito)
-    jne     CONT                ; jump caso ZF = 0
+    jne     CONT                ; jump caso ZF = 0 (neste caso seria ate possivel fazer o contrario e so ter jcxz aqui)
     inc     ebx                 ; ebx e utilizado como counter de 'a's
     jmp     DENOVOA
 
@@ -61,16 +61,16 @@ Q2FIM:
 
 
 Q3:
-    xor     ebx,    ebx     ; zerar ebx para poder usar como incremento ao criar a string inversa
-    mov     ecx,    47      ; fim da string (ultimo 'o')
+    xor     ebx,    ebx         ; zerar ebx para poder usar como incremento ao criar a string inversa
+    mov     ecx,    47          ; fim da string (ultimo 'o')
 
 Q3INVRT:
-    cmp     ecx,    6       ; checar se estamos no comeco da string - 1 (o espaco antes do primeiro 'a')
-    je      Q3FIM           ; caso sim, terminar
+    cmp     ecx,    6           ; checar se estamos no comeco da string - 1 (o espaco antes do primeiro 'a')
+    je      Q3FIM               ; caso sim, terminar
     mov     al,    [frase+ecx]  ; mov [Q3MSG+ebx], [frase+ecx] nao pode ser feito diretamente, portanto
     mov     [msg+ebx], al       ; utilizamos al como buffer para realizar essa troca, um byte por vez
-    inc ebx                 ; proximo caractere a ser colocado na string a imprimir
-    dec ecx                 ; caractere anterior a ser lido da string normal
+    inc ebx                     ; proximo caractere a ser colocado na string a imprimir
+    dec ecx                     ; caractere anterior a ser lido da string normal
     jmp Q3INVRT
 
 Q3FIM:
